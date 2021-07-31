f4ef3e7b8fc419ad51df918544daccb4e959b9ba
Carolina Pirela <caropi30@gmail.com>
2021-07-31 08:36:28 -0400

    subiendo ejercicios eloquent looping-triangle:fizz-buzz y archivo README

---
+++ README.md
@@ -0,0 +1,3 @@
+##ELOQUENT JS EXCERCISES
+
+Es repositorio contiene respuestas para los ejercicios presentados en el libro Eloquent Javascript. Un libro necesario para todo el que quiera incursionar en el mundo de JS.
---
+++ js/looping-triangle.js
@@ -0,0 +1,8 @@
+let triangle = '########';
+let triangleLength = triangle.length;
+
+let fullTriangleAfterLoop = '';
+
+for(let i = 0; i < triangleLength; i++) {
+    console.log(fullTriangleAfterLoop += triamgle[i])
+}
